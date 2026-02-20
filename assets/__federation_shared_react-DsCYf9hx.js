7d3070e5982bc2882516f2aefea0093f1f8b8e99
v2lentina <v2lentina@users.noreply.github.com>
2026-02-20 17:00:17 +0000

    deploy: v2lentina/gaia@d69d2c2c751b1239edaa71a1be4213f271148188

---
+++ assets/__federation_shared_react-DsCYf9hx.js
@@ -0,0 +1,6 @@
+import { r as requireReact, g as getDefaultExportFromCjs } from './index-DKl3hPDx.js';
+
+var reactExports = requireReact();
+const index = /*@__PURE__*/getDefaultExportFromCjs(reactExports);
+
+export { index as default };
---
+++ assets/__federation_shared_react-dom-C4-8MelP.js
@@ -0,0 +1,7 @@
+import { g as getDefaultExportFromCjs } from './index-DKl3hPDx.js';
+import { r as requireReactDom } from './index-XG8H9oNv.js';
+
+var reactDomExports = requireReactDom();
+const index = /*@__PURE__*/getDefaultExportFromCjs(reactDomExports);
+
+export { index as default };
